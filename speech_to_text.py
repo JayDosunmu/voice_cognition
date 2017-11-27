a31f66e2f699a738cd139d4c4c5748a0f065c074
TJDosunmu <theblasiantj@gmail.com>
2017-11-27 06:13:17 -0500

    practicing with google speech recognition

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,1 +1,26 @@
 import io
+import os
+
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+client = speech.SpeechClient()
+
+file_name = os.path.join(os.path.dirname(__file__),
+    'nommel_sample.wav')
+
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en_US'
+)
+
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternativees[0].transcript))
