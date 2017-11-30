65f4fce2a53a836776a23fe070385a50deecac39
TJDosunmu <theblasiantj@gmail.com>
2017-11-30 09:50:34 -0500

    further refine ms api calls

--- speaker_recognition/enroll_profile_id.py
+++ speaker_recognition/enroll_profile_id.py
@@ -3,17 +3,19 @@ import http.client, urllib.request, urllib.parse, urllib.error, base64
 headers = {
     # Request headers
     'Content-Type': 'multipart/form-data',
-    'Ocp-Apim-Subscription-Key': '{subscription key}',
+    'Ocp-Apim-Subscription-Key': 'e5e05379080c473eab00638e44ed3ad4',
 }
 
 params = urllib.parse.urlencode({
     # Request parameters
-    'shortAudio': '{boolean}',
+    'shortAudio': 'false',
 })
+
+body = 
 
 try:
     conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/spid/v1.0/identificationProfiles/{identificationProfileId}/enroll?%s" % params, "{body}", headers)
+    conn.request("POST", "/spid/v1.0/identificationProfiles/e5e05379080c473eab00638e44ed3ad4/enroll?%s" % params, "{body}", headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
